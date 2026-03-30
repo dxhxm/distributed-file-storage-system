c686b70a82ab58d21dc4b5328a7bf8500e9ab234
hiruni <it24610786@my.sliit.lk>
2026-03-30 15:48:06 +0530

    Implemented data replication with alive node check and failure handling

--- app/api/replicate.py
+++ app/api/replicate.py
@@ -1,6 +1,32 @@
-@app.route('/replicate', methods=['POST'])
-def replicate():
-    file = request.files['file']
-    file.save(f"Storage/{file.filename}")
+import requests
+from app.models.config import NODES, CURRENT_NODE
 
-    return {"message": "File replicated successfully"}
+#Check node is alive
+def is_node_alive(node):
+    try:
+        res = requests.get(f"{node}/health", timeout=2)
+        return res.status_code == 200
+    except:
+        return False
+
+
+def replicate_file(file_path, filename):
+    for node in NODES:
+
+        if node == CURRENT_NODE:
+            continue  
+
+        #Skip dead nodes
+        if not is_node_alive(node):
+            print(f"git add .{node} is DOWN - skipping")
+            continue
+
+        try:
+            with open(file_path, 'rb') as f:
+                files = {'file': (filename, f)}
+                response = requests.post(f"{node}/replicate", files=files)
+
+            print(f"Sent to {node} - {response.status_code}")
+
+        except Exception as e:
+            print(f" Failed to send to {node}: {e}")
